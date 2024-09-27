f7d167980600869869359e9a4c8bda6a4a222845
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-09-27 20:34:41 +0200

    Tilføjet Console.WriteLine til at logge hvad Klienten sender + Kommentarer som forklarer koden

--- TCPServerOblOpg4/Program.cs
+++ TCPServerOblOpg4/Program.cs
@@ -18,6 +18,7 @@ while (true)
 
 // listener.Stop();
 
+// Method to handle client connections
 void HandleClient(TcpClient socket)
 {
     NetworkStream ns = socket.GetStream();
@@ -29,7 +30,7 @@ void HandleClient(TcpClient socket)
     while (socket.Connected)
     {
         string message = reader.ReadLine().ToLower(); // Modtager besked fra client (i lowercase)
-        Console.WriteLine("Besked modtaget:" + message);
+        Console.WriteLine("Besked modtaget: " + message);
 
         counter++;
 
@@ -37,64 +38,18 @@ void HandleClient(TcpClient socket)
         {
             case "stop":
                 HandleStop(writer, socket);
-                //writer.WriteLine("Goodbye: Closing down Client");
-                //writer.Flush();
-                //socket.Close();
                 break;
 
             case "random":
                 HandleRandom(reader, writer);
-                //writer.WriteLine("Random number x and y");
-                //writer.Flush();
-
-                //string[] messageA = reader.ReadLine().Split(" ");
-                //int x = Int32.Parse(messageA[0]);
-                //int y = Int32.Parse(messageA[1]);
-                //Console.WriteLine(x);
-                //Console.WriteLine(y);
-
-                //int minValue = Math.Min(x, y);
-                //int maxValue = Math.Max(x, y);
-
-                //Random random = new Random();
-                //int n = random.Next(minValue, maxValue + 1); // +1 for at inkludere maxValue i det tilfældige interval: Random.Next()
-                //string sn = n.ToString();
-                //writer.WriteLine(sn);
-                //writer.Flush();
                 break;
 
             case "add":
                 HandleAdd(reader, writer);
-                //writer.WriteLine("Add two numbers");
-                //writer.Flush();
-
-                //string[] messageB = reader.ReadLine().Split(" ");
-                //int a = Int32.Parse(messageB[0]);
-                //int b = Int32.Parse(messageB[1]);
-                //Console.WriteLine(a);
-                //Console.WriteLine(b);
-
-                //int sum = a + b;
-                //string samletSum = sum.ToString();
-                //writer.WriteLine(samletSum);
-                //writer.Flush();
                 break;
 
             case "subtract":
                 HandleSubtract(reader, writer);
-                //writer.WriteLine("Subtract two numbers");
-                //writer.Flush();
-
-                //string[] messageC = reader.ReadLine().Split(" ");
-                //int c = Int32.Parse(messageC[0]);
-                //int d = Int32.Parse(messageC[1]);
-                //Console.WriteLine(c);
-                //Console.WriteLine(d);
-
-                //int forskel = c - d;
-                //string samletForskel = forskel.ToString();
-                //writer.WriteLine(samletForskel);
-                //writer.Flush();
                 break;
 
             default:
@@ -104,24 +59,24 @@ void HandleClient(TcpClient socket)
         }
     }
 }
-
+// Method to handle the "stop" command
 void HandleStop(StreamWriter writer, TcpClient socket)
 {
     writer.WriteLine("Goodbye: Closing down Client");
     writer.Flush();
+    Console.WriteLine("Client connection closed.");
     socket.Close();
 }
 
+// Method to handle the "random" command
 void HandleRandom(StreamReader reader, StreamWriter writer)
 {
-    writer.WriteLine("Random number x and y");
+    writer.WriteLine("Enter two random numbers: x y");
     writer.Flush();
 
     string[] messageA = reader.ReadLine().Split(" ");
     int x = Int32.Parse(messageA[0]);
     int y = Int32.Parse(messageA[1]);
-    Console.WriteLine(x);
-    Console.WriteLine(y);
 
     int minValue = Math.Min(x, y);
     int maxValue = Math.Max(x, y);
@@ -129,43 +84,56 @@ void HandleRandom(StreamReader reader, StreamWriter writer)
     Random random = new Random();
     int n = random.Next(minValue, maxValue + 1); // +1 for at inkludere maxValue i det tilfældige interval: Random.Next()
     string sn = n.ToString();
+
+    // Logging the operation and result to the console
+    Console.WriteLine($"Tilfældig nummer mellem {x} og {y} er: {n}");
+    Console.WriteLine($"resultatet sendt: {sn}");
+
+    // Sending the result back to the client
     writer.WriteLine(sn);
     writer.Flush();
 }
 
+// Method to handle the "add" command
 void HandleAdd(StreamReader reader, StreamWriter writer)
 {
-    writer.WriteLine("Add two numbers");
+    writer.WriteLine("Add two numbers: E.g: 1 2");
     writer.Flush();
 
     string[] messageB = reader.ReadLine().Split(" ");
     int a = Int32.Parse(messageB[0]);
     int b = Int32.Parse(messageB[1]);
-    //Console.WriteLine(a);
-    //Console.WriteLine(b);
 
     int sum = a + b;
     string samletSum = sum.ToString();
 
+    // Logging the operation and result to the console
     Console.WriteLine($"{a} + {b} = {sum}");
     Console.WriteLine($"resultatet sendt: {samletSum}");
+
+    // Sending the result back to the client
     writer.WriteLine(samletSum);
     writer.Flush();
 }
 
+// Method to handle the "subtract" command
 void HandleSubtract(StreamReader reader, StreamWriter writer)
 {
-    writer.WriteLine("Enter 2 numbers with space in between numbers to Subtract");
+    writer.WriteLine("Subtract two numbers: E.g: 5 2");
     writer.Flush();
 
     string[] messageC = reader.ReadLine().Split(" ");
     int c = Int32.Parse(messageC[0]);
     int d = Int32.Parse(messageC[1]);
-    Console.WriteLine(c);
-    Console.WriteLine(d);
 
     int forskel = c - d;
     string samletForskel = forskel.ToString();
+
+    // Logging the operation and result to the console
+    Console.WriteLine($"{c} - {d} = {forskel}");
+    Console.WriteLine($"resultatet sendt: {samletForskel}");
+
+    // Sending the result back to the client
     writer.WriteLine(samletForskel);
     writer.Flush();
 }
